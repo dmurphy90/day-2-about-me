f24da494022cc3b653ee257a46a32ac4e5dd1fe2
Matt Harding <matt@wherethehellismatt.com>
2017-10-19 15:35:04 -0700

    put questions in functions

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # day-2-about-me
 Day 2 App
+
+Driver: Matt Harding
+Navigator: Dean Murphy
+
+We made this together.
--- app.js
+++ app.js
@@ -6,83 +6,112 @@ var user = prompt('Greetings aspiring Historian of all things Dean. May I have y
 console.log('Users name:', user);
 
 alert('Welcome ' + user + ' let us begin uncovering the secrets of Dean. You will be asked a series of Yes or No questions at first.');
+
+function birthQuestion() {
+  var birthLower = birth.toLowerCase();
+  console.log('Birth answer:', birthLower);
+  console.log('Score:', score);
+  //
+  if (birthLower === 'yes' || birthLower === 'y') {
+    score++;
+    alert('No ' + user + ', it\'s the 21st Century. He was delivered in a hospital by a doctor.');
+  } else {
+    alert('CORRECT! Off to a good start ' + user + '.');
+  }
+}
+
 // // first question
 var birth = prompt('Was Dean delivered in a barn by a nearby farmer?');
-var birthLower = birth.toLowerCase();
-console.log('Birth answer:', birthLower);
-console.log('Score:', score);
-//
-if (birthLower === 'yes' || birthLower === 'y') {
-  score++;
-  alert('No ' + user + ', it\'s the 21st Century. He was delivered in a hospital by a doctor.');
-} else {
-  alert('CORRECT! Off to a good start ' + user + '.');
+birthQuestion();
+
+function dogsQuestion() {
+  var dogsLower = dogs.toLowerCase();
+  console.log('Dog lover:', dogsLower);
+  console.log('Score:', score);
+
+  if (dogsLower === 'yes' || dogsLower === 'y') {
+    alert('CORRECT! Of course you knew that he does ' + user + '! Anybody who knows Dean knows that his own dog Winnie is the most important thing in the world to him!');
+    score++;
+  } else {
+    alert('FALSE! Wow, ' + user + '. What does Dean look like to you? A cat person?');
+  }
 }
+
 // //second question
 var dogs = prompt('Does Dean love dogs?');
-var dogsLower = dogs.toLowerCase();
-console.log('Dog lover:', dogsLower);
-console.log('Score:', score);
+dogsQuestion();
 
-if (dogsLower === 'yes' || dogsLower === 'y') {
-  alert('CORRECT! Of course you knew that he does ' + user + '! Anybody who knows Dean knows that his own dog Winnie is the most important thing in the world to him!');
-  score++;
-} else {
-  alert('FALSE! Wow, ' + user + '. What does Dean look like to you? A cat person?');
+function travelQuestion() {
+  var travelLower = travel.toLowerCase();
+  console.log('Travel answer:', travelLower);
+
+  if (travelLower === 'yes' || travelLower === 'y') {
+    alert('DING DING DING! He drove from Florida to Seattle with his co-captain Winnie!');
+    score++;
+  } else {
+    alert('FALSE! He drove to Seattle from Florida to attend CodeFellows and learn how to write this very code.');
+  }
 }
+
 // //third question
 var travel = prompt('Has Dean driven across the United States of America?');
-var travelLower = travel.toLowerCase();
-console.log('Travel answer:', travelLower);
+travelQuestion();
 
-if (travelLower === 'yes' || travelLower === 'y') {
-  alert('DING DING DING! He drove from Florida to Seattle with his co-captain Winnie!');
-  score++;
-} else {
-  alert('FALSE! He drove to Seattle from Florida to attend CodeFellows and learn how to write this very code.');
+function grandmaQuestion() {
+  var grandmaLower = grandma.toLowerCase();
+  console.log('Grandma rules:', grandmaLower);
+  if (grandmaLower === 'yes' || grandmaLower === 'y') {
+    alert('CORRECT! Of course he is, she\'s the best!');
+  } else {
+    alert('FALSE! What\'s the matter ' + user + '? Old people can\'t be cool too?');
+  }
 }
+
 // //fourth question
 var grandma = prompt('Is Dean\'s best friend his grandmother?');
-var grandmaLower = grandma.toLowerCase();
-console.log('Grandma rules:', grandmaLower);
+grandmaQuestion();
 
-if (grandmaLower === 'yes' || grandmaLower === 'y') {
-  alert('CORRECT! Of course he is, she\'s the best!');
-} else {
-  alert('FALSE! What\'s the matter ' + user + '? Old people can\'t be cool too?');
+function skateQuestion() {
+  var skateLower = skate.toLowerCase();
+  console.log('Skater:', skateLower);
+
+  if (skateLower === 'yes' || skateLower === 'y') {
+    alert('FALSE! While the praise is welcome, no he is not at all. Brian is the superior skater. Dean is merely his student trying to butter him up with questions like this.');
+  } else {
+    alert('CORRECT! Dean could never surpass the great Brian Nations in skating, ever. Don\'t ask him to try... it\'s bad.');
+  }
 }
+
 // //fifth question
 var skate = prompt('Is Dean an accomplished skater, surpassing even the great Brian Nations?');
-var skateLower = skate.toLowerCase();
-console.log('Skater:', skateLower);
+skateQuestion();
 
-if (skateLower === 'yes' || skateLower === 'y') {
-  alert('FALSE! While the praise is welcome, no he is not at all. Brian is the superior skater. Dean is merely his student trying to butter him up with questions like this.');
-} else {
-  alert('CORRECT! Dean could never surpass the great Brian Nations in skating, ever. Don\'t ask him to try... it\'s bad.');
-}
 // //favorite number guessing game
 var number;
 var numGuesses = 0;
 
-while (numGuesses < 4) {
-  number = parseInt(prompt('Tell me ' + user + ' what is Dean\'s favorite number?'));
+function numberQuestion() {
+  while (numGuesses < 4) {
+    number = parseInt(prompt('Tell me ' + user + ' what is Dean\'s favorite number?'));
 
-  if (number < 13) {
-    alert('Not quite there yet ' + user + ' try something a little higher');
-    numGuesses++;
-  } else if (number > 13) {
-    alert('Ooh a little too high with that one ' + user + ' try something a little lower');
-    numGuesses++;
-  } else if (isNaN(number)) {
-    alert('Come on now ' + user + ' I asked for a NUMBER!');
-  } else {
-    numGuesses = 4;
-    alert('BINGO! Excellent guess ' + user + '!');
+    if (number < 13) {
+      alert('Not quite there yet ' + user + ' try something a little higher');
+      numGuesses++;
+    } else if (number > 13) {
+      alert('Ooh a little too high with that one ' + user + ' try something a little lower');
+      numGuesses++;
+    } else if (isNaN(number)) {
+      alert('Come on now ' + user + ' I asked for a NUMBER!');
+    } else {
+      numGuesses = 4;
+      alert('BINGO! Excellent guess ' + user + '!');
+    }
   }
+
+  console.log('Favorite Number Guesses:', numGuesses);
 }
 
-console.log('Favorite Number Guesses:', numGuesses);
+numberQuestion();
 
 //seventh question with multiple answers
 var interests = ['music', 'dogs', 'food', 'gaming', 'roadtrips', 'fitness'];
@@ -90,21 +119,24 @@ var answer;
 var intGuesses = 0;
 var flag;
 
+function interestsQuestion() {
+  while (intGuesses < 6) {
+    answer = prompt('What do you think some of Dean\'s interests are, ' + user + '?');
+    intGuesses++;
+    for (var i = 0; i < interests.length; i++) {
+      console.log('Interests Guesses:', intGuesses);
 
-while (intGuesses < 6) {
-  answer = prompt('What do you think some of Dean\'s interests are, ' + user + '?');
-  intGuesses++;
-  for (var i = 0; i < interests.length; i++) {
-    console.log('Interests Guesses:', intGuesses);
-
-    if (answer.toLowerCase() === interests[i]) {
-      alert('Correct ' + user + '! His interests include: Music, Dogs, Food, Gaming, Roadtrips, and Fitness!');
-      flag = true;
-      intGuesses = 7;
-    }
-    // i - is current index
-    if (i === interests.length && flag === false) {
-      alert('You were incorrect');
+      if (answer.toLowerCase() === interests[i]) {
+        alert('Correct ' + user + '! His interests include: Music, Dogs, Food, Gaming, Roadtrips, and Fitness!');
+        flag = true;
+        intGuesses = 7;
+      }
+      // i - is current index
+      if (i === interests.length && flag === false) {
+        alert('You were incorrect');
+      }
     }
   }
 }
+
+interestsQuestion();
